ba24e9d1ee3be5098470a7f05f902957fd96669d
maanemann <hans@niila.io>
2024-04-07 21:27:28 +0200

    Remove unused useEffect import in  context.js & delete Lauphex.js

--- app/context.js
+++ app/context.js
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState } from "react";
 
 
 // // #region themeContext

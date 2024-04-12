6056d9caea9a533a2312285b44844637a9e3448b
maanemann <hans@niila.io>
2024-04-12 19:43:05 +0200

    Remove failed dynamic tw class (replaced in prev commit). Render line breaks from projectsData as new paragraphs

--- app/projects/[id]/page.js
+++ app/projects/[id]/page.js
@@ -11,27 +11,32 @@ const ProjectDetails = () => {
   const project = data.find(project => project.id === id);
   const imgWidth = project.imgWidth;
   const imgHeight = project.imgHeight;
-  const twAspect = 'aspect-[' + imgWidth + '/' + imgHeight + ']';
   return ( <>
       <h1 className="text-3xl font-bold">{project.title}</h1>
-      <p>{project.description}</p>
-        <div
-          className={`relative w-full ${twAspect}`}
-          style={{
-            // Leaves a space for the image to fill. In CSS, percentage-based padding is calculated relative to the width of the parent :
-            paddingBottom: `${imgHeight / imgWidth * 100}%`
-          }}
-        >
-          <Image
-            src={project.imgSrc} alt={project.imgAlt}
-            priority fill sizes="100vw"
-            className="w-full object-contain object-left-top
-              opacity-0
-            "
-            onLoad={(e) =>
-              e.target.classList.remove('opacity-0')
-            }
-          />
+      {/* `\n` is a "newline character" used in projectsData.json, and with the split method, the string is split into an array of substrings, which is then mapped over to render each substring as a paragraph
+       */}
+      <div className='grid gap-1'>
+        {project.description.split('\n').map((paragraph, index) => (
+          <p key={index} className="paragraph">{paragraph}</p>
+        ))}
+      </div>
+      <div
+        className='relative w-full'
+        style={{
+          // Leaves a space for the image to fill. In CSS, percentage-based padding is calculated relative to the width of the parent :
+          paddingBottom: `${imgHeight / imgWidth * 100}%`
+        }}
+      >
+        <Image
+          src={project.imgSrc} alt={project.imgAlt}
+          priority fill sizes="100vw"
+          className="w-full object-contain object-left-top
+            opacity-0
+          "
+          onLoad={(e) =>
+            e.target.classList.remove('opacity-0')
+          }
+        />
       </div>
   </> );
 }

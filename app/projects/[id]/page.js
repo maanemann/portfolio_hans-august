996b216410bf969e17e1e8659c5687c2c4e2ad45
maanemann <hans@niila.io>
2024-04-12 20:23:25 +0200

    Refactor projectDetails to use theme context text color

--- app/projects/[id]/page.js
+++ app/projects/[id]/page.js
@@ -4,21 +4,26 @@
 import data from '@/data/projectsData.json';
 import { useParams } from 'next/navigation';
 import Image from 'next/image';
+import { useThemeContext } from '@/app/context';
 
 
 const ProjectDetails = () => {
   const { id } = useParams();
+  const { theme } = useThemeContext();
   const project = data.find(project => project.id === id);
   const imgWidth = project.imgWidth;
   const imgHeight = project.imgHeight;
   return ( <>
-      <h1 className="text-3xl font-bold">{project.title}</h1>
-      {/* `\n` is a "newline character" used in projectsData.json, and with the split method, the string is split into an array of substrings, which is then mapped over to render each substring as a paragraph
-       */}
-      <div className='grid gap-1'>
-        {project.description.split('\n').map((paragraph, index) => (
-          <p key={index} className="paragraph">{paragraph}</p>
-        ))}
+      <div className={`${theme.textATheme}`}
+      >
+        <h1 className="text-3xl font-bold">{project.title}</h1>
+        {/* `\n` is a "newline character" used in projectsData.json, and with the split method, the string is split into an array of substrings, which is then mapped over to render each substring as a paragraph
+        */}
+        <div className='grid gap-1'>
+          {project.description.split('\n').map((paragraph, index) => (
+            <p key={index} className="paragraph">{paragraph}</p>
+          ))}
+        </div>
       </div>
       <div
         className='relative w-full'

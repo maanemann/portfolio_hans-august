3498f464c3303f4cc04294e8f1032b77a0a66b9f
maanemann <hans@niila.io>
2024-05-01 13:28:24 +0200

    Improve about page content + layout

--- app/about/page.js
+++ app/about/page.js
@@ -5,7 +5,7 @@ import Article from "@/components/Article";
 import { H1 } from "@/components/Headings";
 import InlLink from "@/components/InlLink";
 import Columns from "@/components/Columns";
-import Paragraph from "@/components/Paragraph";
+import Paragraph, { PNoBrIns } from "@/components/Paragraph";
 
 const About = () => {
   return ( <>
@@ -18,11 +18,15 @@ const About = () => {
           <Paragraph>
             I&apos;m a front-end developer / UI + UX designer located in Copenhagen, Denmark.
           </Paragraph>
+          <div></div>
           <Paragraph>
-            User experience is the sum of presentation, functionality and performance. I think of interfaces as entrances into virtual rooms for the user to explore. Just like entering eg. a hotel lobby, you should feel comfortable and intrigued. The room / UI should have a clear identity, and it should be easy to find what you&apos;re looking for without inconvenient barriers.
+            User experience is the sum of presentation, functionality and performance. I think of interfaces as entrances into virtual rooms for the user to explore.
           </Paragraph>
+          <PNoBrIns>
+            When entering, you should feel comfortable and intrigued, the room should have an appealing identity and communicate where it wants to take you next. It should be easy to find what you&apos;re looking for without inconvenient barriers.
+          </PNoBrIns>
           <Paragraph>
-            Have a look at my <InlLink href="/cv">CV</InlLink> to get a better idea of the tools I use and the projects I&apos;ve worked on. If you have questions or want to involve me in a project, send me a mail at <InlLink href="mailto:hej@hansaugust.com"> hej@hansaugust.com</InlLink>. I enjoy working with inspiring people of all kinds of professions.
+            Have a look at my <InlLink href="/cv">CV</InlLink> to get a better idea of the tools I use and the projects I&apos;ve worked on. If you want to involve me in a project or just want to say hi, send me a mail at <InlLink href="mailto:hej@hansaugust.com"> hej@hansaugust.com</InlLink>.
           </Paragraph>
         </Columns>
       </div>
--- components/Paragraph.js
+++ components/Paragraph.js
@@ -1,11 +1,27 @@
+
+const defaultParagraph = "orphansCustom mb-4 last:mb-0 text-pretty";
+
 const Paragraph = ({ children }) => {
   return ( 
-    <p className="
-      orphansCustom mb-4 last:mb-0 text-pretty
-    ">
+    <p className={`
+      ${defaultParagraph}
+    `}>
       { children }
     </p>
    );
 }
- 
+
 export default Paragraph;
+
+const PNoBrIns = ({ children }) => {
+  return (
+    <p className={`
+      ${defaultParagraph}
+      break-inside-avoid
+    `}>
+      { children }
+    </p>
+   );
+}
+
+export {PNoBrIns};

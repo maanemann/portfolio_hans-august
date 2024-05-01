a89988daf4b1b89e62e0274265ee59e8663c9cf9
maanemann <hans@niila.io>
2024-05-01 09:53:46 +0200

    Fix `'` error from vercel (no this should't have been in the main branch)

--- app/about/page.js
+++ app/about/page.js
@@ -16,13 +16,13 @@ const About = () => {
         </H1>
         <Columns>
           <Paragraph>
-            I'm a front-end developer / UI + UX designer located in Copenhagen, Denmark.
+            I&apos;m a front-end developer / UI + UX designer located in Copenhagen, Denmark.
           </Paragraph>
           <Paragraph>
-            User experience is the sum of presentation, functionality and performance. I think of interfaces as entrances into virtual rooms for the user to explore. Just like entering eg. a hotel lobby, you should feel comfortable and intrigued. The room / UI should have a clear identity, and it should be easy to find what you're looking for without inconvenient barriers.
+            User experience is the sum of presentation, functionality and performance. I think of interfaces as entrances into virtual rooms for the user to explore. Just like entering eg. a hotel lobby, you should feel comfortable and intrigued. The room / UI should have a clear identity, and it should be easy to find what you&apos;re looking for without inconvenient barriers.
           </Paragraph>
           <Paragraph>
-            Have a look at my <InlLink href="/cv">CV</InlLink> to get a better idea of the tools I use and the projects I've worked on. If you have questions or want to involve me in a project, send me a mail at <InlLink href="mailto:hej@hansaugust.com"> hej@hansaugust.com</InlLink>. I enjoy working with inspiring people of all kinds of professions.
+            Have a look at my <InlLink href="/cv">CV</InlLink> to get a better idea of the tools I use and the projects I&apos;ve worked on. If you have questions or want to involve me in a project, send me a mail at <InlLink href="mailto:hej@hansaugust.com"> hej@hansaugust.com</InlLink>. I enjoy working with inspiring people of all kinds of professions.
           </Paragraph>
         </Columns>
       </div>

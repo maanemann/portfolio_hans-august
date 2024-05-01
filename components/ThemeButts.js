40aecc79a83e43644e393c912912f2af1e86dc11
maanemann <hans@niila.io>
2024-05-01 15:23:16 +0200

    Style theme buttons w hover etc

--- components/ThemeButts.js
+++ components/ThemeButts.js
@@ -4,6 +4,12 @@ import { useThemeContext } from "@/app/context";
 const ThemeButts = () => {
   const { setTheme } = useThemeContext();
 
+  const commonProps = `
+    w-6 aspect-square cursor-pointer
+    outline outline-2 rounded-full
+    `;
+    // scale-y-75    hover:scale-y-100
+
   return ( 
     <div className="
         absolute bottom-6 left-6
@@ -11,16 +17,18 @@ const ThemeButts = () => {
       ">
         <div
           onClick={() => setTheme('ockerdust')}
-          className="
-            w-6 aspect-square cursor-pointer
-            bg-ockerdust-700 rounded-full
-        "/>
+          className={`
+            ${commonProps}
+            bg-ockerdust-600        hover:bg-ockerdust-500
+            outline-ockerdust-800   hover:outline-ockerdust-700
+        `}/>
         <div
           onClick={() => setTheme('aqua')}
-          className="
-            w-6 aspect-square cursor-pointer
-            bg-aqua-800 rounded-full
-        "/>
+          className={`
+            ${commonProps}
+            bg-aqua-800        hover:bg-aqua-700
+            outline-aqua-900   hover:outline-aqua-800
+        `}/>
       </div>
    );
 }

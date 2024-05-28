e64f3dd796a3505b6043895307c7291c641402df
maanemann <hans@niila.io>
2024-05-28 17:11:39 +0200

    Remove projects hover on touch, fix hover before load, fix text not fully gone when hovering

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -4,18 +4,29 @@
 import Image from "next/image";
 import { useThemeContext } from "@/app/context";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const ProjectItem = ({ title, imgSrc, imgAlt, link }) => {
   const { theme } = useThemeContext();
+  const [pointerType, setPointerType] = useState(null);
+
+  // tjek om der bruges touch device (condition false) elle ej (condition true). useEffect sikrer at koden ikke kører på serveren, hvor `window` ikke eksisterer :
+  useEffect(() => {
+    if (window.matchMedia("(pointer: fine)").matches) {
+      setPointerType("group");
+    }
+  }, []);
 
   return ( <figure className={`
     relative w-full h-[16rem] xl:h-[24rem] 2xl:h-[32rem]
     ${theme.bgBrightBTheme}
     coolCornersCustom
   `}>
-    <Link href={ link } className="
-      absolute w-full h-full group overflow-hidden
-    ">
+    {/* her printes `pointerType` variablens indhold ("group") som class : */}
+    <Link href={ link } className={`
+      absolute w-full h-full overflow-hidden
+      ${pointerType}
+    `}>
       <p className={`
         block mt-6 ml-12 opacity-65
         text-4xl font-bold tracking-wider italic ${theme.textA2Theme}
@@ -28,11 +39,9 @@ const ProjectItem = ({ title, imgSrc, imgAlt, link }) => {
         className="
           opacity-0
           absolute object-cover
-          hover:grayscale-0
-          group-hover:opacity-100
         "
         onLoad={(e) => {
-          e.target.classList.add('opacity-55');
+          e.target.classList.add('opacity-65', 'group-hover:opacity-100');
           e.target.classList.remove('opacity-0');
           e.target.previousElementSibling.remove();
           e.target.nextElementSibling.classList.remove('opacity-0');
@@ -49,9 +58,9 @@ const ProjectItem = ({ title, imgSrc, imgAlt, link }) => {
         <h2 className={`
           absolute bottom-0 text-nowrap my-1 mx-3
           text-3xl font-bold lowercase tracking-wider
-          group-hover:-my-16 group-hover:-mx-52
-          group-hover:text-7xl ease-in-out duration-150
           ${theme.textATheme}
+          group-hover:-my-32 group-hover:-mx-52
+          group-hover:text-7xl ease-in-out duration-150
         `}>
           { title }
         </h2>
